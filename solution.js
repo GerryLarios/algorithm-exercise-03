d38008d30e375342f155b4409c2981fc80aa25d1
Gerardo Larios <gerardo.larios69@gmail.com>
2021-06-04 16:55:08 -0500

    [WIP] improve solution

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
-const bubbleSort = (list = []) => {
+const bubbleSort = (list = []) => { // O(n ^ 2)
+  if (!list.length) return list
+  
+  if (list.length === 2) {
+    if (list[0] < list[1]) return list
+    else [list[1], list[0]]
+  }
+
   const lenght = list.length
-  for (let i = 0; i < lenght; i = i + 1) {
+  for (let i = 0; i < lenght; i = i + 1) { 
     for (let j = 0; j < lenght; j = j + 1) {
       if (list[j] > list[j + 1]) {
         let tmp = list[j]
@@ -8,12 +15,15 @@ const bubbleSort = (list = []) => {
         list[j + 1] = tmp
       }
     }
-  }
+  } 
   return list
 }
 
+const binarySearch = (list = [], find = 0, leftIndex = 0, rightIndex = 0) => { // theta (log(n))
 
-const binarySearch = (list = [], find = 0, leftIndex = 0, rightIndex = 0) => {
+  if (!list.length) return -1
+  if (list.length === 1 && list[0] === find) return 0
+
   if (rightIndex >= leftIndex) {
     const index = leftIndex + Math.floor((rightIndex - leftIndex) / 2)
     const item = list[index]
@@ -28,9 +38,26 @@ const binarySearch = (list = [], find = 0, leftIndex = 0, rightIndex = 0) => {
   return -1
 }
 
+const isSorted = (list = []) => { // O^(n / d)
+  if (!list.length) return true
+  if (list.length === 2) return list[0] < list[1]
+
+  let sorted = true
+
+  for (let i = 0; i < list.lenght; i = i + 1) {
+    if (list[i + 1] && list[i] > list[i + 1]) {
+      sorted = false
+      break
+    }
+  }
+
+  return sorted
+}
+
 const exists = (list = [], find = 0) => {
   if (!list || list?.length === 0) return false
-  return binarySearch(bubbleSort(list), find, 0, list.length - 1) > -1
+  const sortedList = isSorted(list) ? list : bubbleSort(list)
+  return binarySearch(sortedList, find, 0, list.length - 1) > -1
 }
 
 module.exports = { exists }
